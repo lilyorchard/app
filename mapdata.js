3935073323caa159947bb03574a58a782138a237
Glitch (hello-webpage) <none>
2019-06-13 21:20:46 +0000

    🚶🌓 Checkpoint ./index.html:5008187/121:5008196/262 ./test.html:5008196/367 ./usmap.js:5008196/226317 ./mapdata.js:5008196/8696 ./Documentation.html:5008196/380

---
+++ mapdata.js
@@ -0,0 +1,454 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "650", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#002767",
+    hide_labels: "no",
+    border_color: "white",
+    
+    //State defaults
+    state_description: "Learn about the products available in your state.",
+    state_color: "#77bcff",
+    state_hover_color: "#038EEB",
+    state_url: "http://simplemaps.com",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "",
+    label_size: "",
+    label_font: "",
+    popups: "detect"
+  },
+  state_specific: {
+    HI: {
+      inactive: "no",
+      hide: "no",
+      name: "Hawaii",
+      description: " "
+    },
+    AK: {
+      inactive: "no",
+      hide: "no",
+      name: "Alaska",
+      description: " "
+    },
+    FL: {
+      inactive: "no",
+      hide: "no",
+      name: "Florida",
+      description: " "
+    },
+    NH: {
+      inactive: "no",
+      hide: "no",
+      name: "New Hampshire",
+      description: " "
+    },
+    VT: {
+      inactive: "no",
+      hide: "no",
+      name: "Vermont",
+      description: " "
+    },
+    ME: {
+      inactive: "no",
+      hide: "no",
+      name: "Maine",
+      description: " "
+    },
+    RI: {
+      inactive: "no",
+      hide: "no",
+      name: "Rhode Island",
+      description: " "
+    },
+    NY: {
+      inactive: "no",
+      hide: "no",
+      name: "New York",
+      description: " "
+    },
+    PA: {
+      inactive: "no",
+      hide: "no",
+      name: "Pennsylvania",
+      description: " "
+    },
+    NJ: {
+      inactive: "no",
+      hide: "no",
+      name: "New Jersey",
+      description: " "
+    },
+    DE: {
+      inactive: "no",
+      hide: "no",
+      name: "Delaware",
+      description: " "
+    },
+    MD: {
+      inactive: "no",
+      hide: "no",
+      name: "Maryland",
+      description: " "
+    },
+    VA: {
+      inactive: "no",
+      hide: "no",
+      name: "Virginia",
+      description: " "
+    },
+    WV: {
+      inactive: "no",
+      hide: "no",
+      name: "West Virginia",
+      description: " "
+    },
+    OH: {
+      inactive: "no",
+      hide: "no",
+      name: "Ohio",
+      description: " "
+    },
+    IN: {
+      inactive: "no",
+      hide: "no",
+      name: "Indiana",
+      description: " "
+    },
+    IL: {
+      inactive: "no",
+      hide: "no",
+      name: "Illinois",
+      description: " "
+    },
+    CT: {
+      inactive: "no",
+      hide: "no",
+      name: "Connecticut",
+      description: " "
+    },
+    WI: {
+      inactive: "no",
+      hide: "no",
+      name: "Wisconsin",
+      description: " "
+    },
+    NC: {
+      inactive: "no",
+      hide: "no",
+      name: "North Carolina",
+      description: " "
+    },
+    DC: {
+      inactive: "no",
+      hide: "no",
+      name: "District of Columbia",
+      description: " "
+    },
+    MA: {
+      inactive: "no",
+      hide: "no",
+      name: "Massachusetts",
+      description: " "
+    },
+    TN: {
+      inactive: "no",
+      hide: "no",
+      name: "Tennessee",
+      description: " "
+    },
+    AR: {
+      inactive: "no",
+      hide: "no",
+      name: "Arkansas",
+      description: " "
+    },
+    MO: {
+      inactive: "no",
+      hide: "no",
+      name: "Missouri",
+      description: " "
+    },
+    GA: {
+      inactive: "no",
+      hide: "no",
+      name: "Georgia",
+      description: " "
+    },
+    SC: {
+      inactive: "no",
+      hide: "no",
+      name: "South Carolina",
+      description: " "
+    },
+    KY: {
+      inactive: "no",
+      hide: "no",
+      name: "Kentucky",
+      description: " "
+    },
+    AL: {
+      inactive: "no",
+      hide: "no",
+      name: "Alabama",
+      description: " "
+    },
+    LA: {
+      inactive: "no",
+      hide: "no",
+      name: "Louisiana",
+      description: " "
+    },
+    MS: {
+      inactive: "no",
+      hide: "no",
+      name: "Mississippi",
+      description: " "
+    },
+    IA: {
+      inactive: "no",
+      hide: "no",
+      name: "Iowa",
+      description: " "
+    },
+    MN: {
+      inactive: "no",
+      hide: "no",
+      name: "Minnesota",
+      description: " "
+    },
+    OK: {
+      inactive: "no",
+      hide: "no",
+      name: "Oklahoma",
+      description: " "
+    },
+    TX: {
+      inactive: "no",
+      hide: "no",
+      name: "Texas",
+      description: " "
+    },
+    NM: {
+      inactive: "no",
+      hide: "no",
+      name: "New Mexico",
+      description: " "
+    },
+    KS: {
+      inactive: "no",
+      hide: "no",
+      name: "Kansas",
+      description: " "
+    },
+    NE: {
+      inactive: "no",
+      hide: "no",
+      name: "Nebraska",
+      description: " "
+    },
+    SD: {
+      inactive: "no",
+      hide: "no",
+      name: "South Dakota",
+      description: " "
+    },
+    ND: {
+      inactive: "no",
+      hide: "no",
+      name: "North Dakota",
+      description: " "
+    },
+    WY: {
+      inactive: "no",
+      hide: "no",
+      name: "Wyoming",
+      description: " "
+    },
+    MT: {
+      inactive: "no",
+      hide: "no",
+      name: "Montana",
+      description: " "
+    },
+    CO: {
+      inactive: "no",
+      hide: "no",
+      name: "Colorado",
+      description: " "
+    },
+    UT: {
+      inactive: "no",
+      hide: "no",
+      name: "Utah",
+      description: " "
+    },
+    AZ: {
+      inactive: "no",
+      hide: "no",
+      name: "Arizona",
+      description: " "
+    },
+    NV: {
+      inactive: "no",
+      hide: "no",
+      name: "Nevada",
+      description: " "
+    },
+    OR: {
+      inactive: "no",
+      hide: "no",
+      name: "Oregon",
+      description: " "
+    },
+    WA: {
+      inactive: "no",
+      hide: "no",
+      name: "Washington",
+      description: " "
+    },
+    CA: {
+      inactive: "no",
+      hide: "no",
+      name: "California",
+      description: " "
+    },
+    MI: {
+      inactive: "no",
+      hide: "no",
+      name: "Michigan",
+      description: " "
+    },
+    ID: {
+      inactive: "no",
+      hide: "no",
+      name: "Idaho",
+      description: " "
+    },
+    GU: {
+      inactive: "no",
+      hide: "yes",
+      name: "Guam",
+      description: " "
+    },
+    VI: {
+      inactive: "no",
+      hide: "yes",
+      name: "Virgin Islands",
+      description: " "
+    },
+    PR: {
+      inactive: "no",
+      hide: "yes",
+      name: "Puerto Rico",
+      description: " "
+    },
+    MP: {
+      inactive: "no",
+      hide: "yes",
+      name: "Northern Mariana Islands",
+      description: " "
+    }
+  },
+  locations: {
+    "0": {
+      name: "New York",
+      lat: "40.71",
+      lng: "-74.0059731",
+      color: "#ffe42f",
+      description: "Planned Parenthood Headquarters",
+      size: "50",
+      type: "star"
+    },
+    "1": {
+      name: "Chicago, IL",
+      lat: "41.9288",
+      lng: "-87.6315",
+      description: "Loop Health Center"
+    },
+    "2": {
+      name: "Seattle, WA",
+      lat: "47.6026",
+      lng: "-122.3284",
+      description: "Central District Health Center"
+    },
+    "3": {
+      name: "San Francisco, CA",
+      lat: "37.7312",
+      lng: "-122.3826",
+      description: "Wohlford Family Clinic at the Good Samaritan Family Resource Center"
+    },
+    "4": {
+      name: "Dallas, TX",
+      lat: "32.7825",
+      lng: "-96.8207",
+      description: "North Dallas Shelburne Health Center"
+    },
+    "5": {
+      name: "Seattle, WA",
+      lat: "47.6026",
+      lng: "-122.3284",
+      description: "First Hill Health Center"
+    },
+    "6": {
+      name: "Atlanta, GA",
+      lat: "33.8477",
+      lng: "-84.2814",
+      description: "East Atlanta Health Center"
+    },
+    "7": {
+      name: "Denver, CO",
+      lat: "39.7525",
+      lng: "-104.9995",
+      description: "Denver Central"
+    },
+    "8": {
+      name: "Minneapolis, MN",
+      lat: "45.0059",
+      lng: "-93.4305",
+      description: "Alexandria Clinic"
+    },
+    "9": {
+      lat: 61.218,
+      lng: -149.858,
+      name: "Anchorage, AK",
+      description: "Anchorage Health Center"
+    },
+    "10": {
+      lat: "19.8968",
+      lng: "155.5828",
+      name: "Honolulu, HI",
+      description: "Honolulu Health Center"
+    }
+  },
+  regions: {},
+  labels: {}
+};
